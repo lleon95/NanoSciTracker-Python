6e99d3b60041bcff54416d8ee91831f729a1523b
Luis Leon <lleon95@gmail.com>
2020-08-11 15:44:23 +0200

    Add the subdetection process

--- src/LocalTracker/main.py
+++ src/LocalTracker/main.py
@@ -9,6 +9,62 @@ import detector
 import tracker
 import drawutils
 
+def add_offset(roi, offset):
+  p1 = (roi[0][0] + offset[0], roi[0][1] + offset[1])
+  p2 = (roi[1][0] + offset[0], roi[1][1] + offset[1])
+  return (p1, p2)
+
+def roi_chopper(gray, bbox):
+  # Crop the image to the roi
+  p1, p2 = bbox
+
+  x1, y1 = p1
+  x2, y2 = p2
+  if x1 < 0:
+    x1 = 0
+  if y1 < 0:
+    y1 = 0
+    
+  roi_gray = gray[y1:y2, x1:x2]
+  
+  return roi_gray, (x1, y1)
+
+def detect_within_roi_tracker(gray, trackers):
+  # Explore trackers
+  detection_offset_bbs = []
+  for tracker in trackers:
+    
+    roi_gray, offset = roi_chopper(gray, tracker.roi)
+    
+    # Detect within roi
+    if roi_gray.shape[0] == 0 or roi_gray.shape[1] == 0:
+      continue
+    
+    size = (int(gray.shape[0] / 2), int(gray.shape[1] / 2))
+    detection_bbs = detector.detect(roi_gray, 1, size)
+    for i in detection_bbs:
+      detection_offset_bbs.append(add_offset(i, offset))
+      
+  return detection_offset_bbs
+
+def detect_within_roi_detection(gray, bbs):
+  # Explore trackers
+  detection_offset_bbs = []
+  for bbox in bbs:
+    
+    roi_gray, offset = roi_chopper(gray, bbox)
+    
+    # Detect within roi
+    if roi_gray.shape[0] == 0 or roi_gray.shape[1] == 0:
+      continue
+    
+    size = (int(gray.shape[0] / 4), int(gray.shape[1] / 4))
+    detection_bbs = detector.detect(roi_gray, 1, size)
+    for i in detection_bbs:
+      detection_offset_bbs.append(add_offset(i, offset))
+      
+  return detection_offset_bbs
+
 def main(args):
   # Open the video
   cap = cv.VideoCapture(args.input)
@@ -39,18 +95,25 @@ def main(args):
       trackers = tracker.deployTrackers(frame, detection_bbs, trackers)
     else:
       tracker.updateTrackers(frame, trackers)
+      
+    # Detection - Per tracker
+    subdetections_tracking = detect_within_roi_tracker(gray_detect2, trackers)
+    subdetections_detector = detect_within_roi_detection(gray_detect3, detection_bbs)
+
     
     # Draw on demand
     if args.draw_detection:
       detections_frame = copy.deepcopy(frame)
-      detections_frame = drawutils.draw_detections(detections_frame, detection_bbs)     
-      detections_frame = cv.resize(detections_frame, (320,240))
+      detections_frame = drawutils.draw_detections(detections_frame, detection_bbs)   
+      detections_frame = drawutils.draw_detections(detections_frame, subdetections_detector, (0,0,255))   
+      #detections_frame = cv.resize(detections_frame, (320,240))
       cv.imshow("Detection", detections_frame)
       
     if args.draw_tracking:
       tracking_frame = copy.deepcopy(frame)
       tracking_frame = drawutils.draw_trackers(tracking_frame, trackers)
-      tracking_frame = cv.resize(tracking_frame, (320,240))
+      tracking_frame = drawutils.draw_detections(tracking_frame, subdetections_tracking, (0,0,255))
+      #tracking_frame = cv.resize(tracking_frame, (320,240))
       cv.imshow("Tracking", tracking_frame)
       
     output_frame = cv.resize(frame, (320,240))
